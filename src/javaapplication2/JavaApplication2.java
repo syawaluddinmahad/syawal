436910e8dcf6534b02ec8f9dd218b61190762edc
syawaluddinmahad <35754454+syawaluddinmahad@users.noreply.github.com>
2018-01-29 10:02:03 +0800

    Update JavaApplication2.java

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication2;
+//Latihan if-else
 
-/**
- *
- * @author Student
- */
-public class JavaApplication2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.*;
+public class Biasiswa { 
+    public static void main (String[] args){
+        String nama;
+        int umur;
+        char bm, bi, mt;
+        double pendapatan;
+        
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Nama: ");
+        nama=input.nextLine();
+        
+        System.out.print("Gred Bahasa Melayu [sila isi A/B/C/D/E]: ");
+        bm=input.next().charAt(0);
+        
+        System.out.print("Gred Bahasa Inggeris [sila isi A/B/C/D/E]: ");
+        bi=input.next().charAt(0);
+        
+        System.out.print("Gred Matematik [sila isi A/B/C/D/E]: ");
+        mt=input.next().charAt(0);
+        
+        System.out.print("Jumlah pendapatan ibu bapa: RM");
+        pendapatan=input.nextDouble();
+        
+        if(bm=='A' && bi=='A' && (mt=='A' || mt=='B' || mt=='C') && pendapatan<2000){
+            System.out.println("Tahniah "+nama+"! Anda layak menerima biasiswa.");
+        }
+        
+        else{
+            System.out.println("Maaf "+nama+", anda tidak layak menerima biasiswa.");
+        }      
     }
-    
 }
